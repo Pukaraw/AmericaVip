c2378ef7bff5ff046a21a3d405d9889c7bd71589
nik-m <nik-m@LAPTOP-SAHQRTFF>
2022-10-16 17:00:24 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVenta.AplicacionWeb/Models/ViewModels/VMCambioClave.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaVenta.AplicacionWeb.Models.ViewModels
+{
+    public class VMCambioClave
+    {
+        public string? claveActual { get; set; }
+        public string? claveNueva { get; set; }
+    }
+}
